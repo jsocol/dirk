113dec81f8272b303dd77579525002d51af4865e
James Socol <james@mozilla.com>
2012-01-26 18:56:42 -0500

    Map to IRC channels.

--- dirk/dirk.py
+++ dirk/dirk.py
@@ -1,17 +1,20 @@
 from django.conf import settings
 import rasputin
 
-from utils import get_channel
+from utils import get_rasputin_channel, get_channel_map
 
 
 def say(channel, message):
     """Post a message to a dirk channel.
 
-    Dirk channels are mapped to one or more IRC channels.
+    Dirk channels are mapped to one or more IRC channels by the
+    DIRK_CHANNEL_MAP setting.
 
     """
+    map_ = get_channel_map()
+    irc_channels = map_.get(channel, [])
     data = {
-        'channel': channel,
+        'channel': irc_channels,
         'message': message
     }
-    rasputin.send(get_channel(), data)
+    rasputin.send(get_rasputin_channel(), data)
--- dirk/utils.py
+++ dirk/utils.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 
 
-DEFAULT_CHANNEL = 'dirk'
+DEFAULT_RASPUTIN = 'dirk'
 
 
-def get_channel():
-    return getattr(settings, 'DIRK_CHANNEL', DEFAULT_CHANNEL)
+def get_channel_map():
+    return getattr(settings, 'DIRK_CHANNEL_MAP', {})
+
+
+def get_rasputin_channel():
+    return getattr(settings, 'DIRK_RASPUTIN_CHANNEL', DEFAULT_RASPUTIN)
