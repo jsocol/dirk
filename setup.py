5a4b7268784033499deb206c5f746771a04a5c56
James Socol <james@mozilla.com>
2012-01-26 16:24:26 -0500

    Add setup.py, README, ignore .egg-info.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import dirk
+
+setup(
+    name='dirk',
+    version=dirk.__version__,
+    description='Provide a bridge between Django and IRC.',
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='https://github.com/jsocol/dirk',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    install_requires=['redis'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
