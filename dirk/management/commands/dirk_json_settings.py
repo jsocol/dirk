ab31892020f7f86d75d0989cb33b9642a1500fe7
James Socol <james@mozilla.com>
2012-01-27 16:04:18 -0500

    Pass all information to node process.

--- dirk/management/commands/dirk_json_settings.py
+++ dirk/management/commands/dirk_json_settings.py
@@ -3,15 +3,11 @@ import json
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 
-from dirk.utils import get_rasputin_channel, get_channel_map
+from dirk.utils import get_node_settings
 
 
 class Command(NoArgsCommand):
     help = 'Dumps dirk settings for the IRC bot.'
 
     def handle_noargs(self, *a, **kw):
-        # TODO(james): Dump Redis connection info.
-        print json.dumps({
-            'channel': get_rasputin_channel(),
-            'map': get_channel_map(),
-        })
+        print json.dumps(get_node_settings())
--- dirk/utils.py
+++ dirk/utils.py
@@ -2,6 +2,8 @@ from django.conf import settings
 
 
 DEFAULT_RASPUTIN = 'dirk'
+DEFAULT_IRC_HOST = 'chat.freenode.net'
+DEFAULT_IRC_NICK = 'dirkbot'
 
 
 def get_channel_map():
@@ -10,3 +12,17 @@ def get_channel_map():
 
 def get_rasputin_channel():
     return getattr(settings, 'DIRK_RASPUTIN_CHANNEL', DEFAULT_RASPUTIN)
+
+
+def get_node_settings():
+    node = {
+        'irc_host': getattr(settings, 'DIRK_IRC_HOST', DEFAULT_IRC_HOST),
+        'irc_nick': getattr(settings, 'DIRK_IRC_NICK', DEFAULT_IRC_NICK),
+        'channels': get_channel_map().keys(),
+        'rasputin_host': getattr(settings, 'RASPUTIN_HOST', 'localhost'),
+        'rasputin_port': getattr(settings, 'RASPUTIN_PORT', 6379),
+        'rasputin_password': getattr(settings, 'RASPUTIN_PASSWORD', None),
+        'rasputin_db': getattr(settings, 'RASPUTIN_REDIS_DB', 0),
+        'rasputin_channel': get_rasputin_channel(),
+    }
+    return node
